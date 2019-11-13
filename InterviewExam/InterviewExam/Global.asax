0f80d17951748bf266bac8f914b09bf843129312
Sichkar Oleg <olegsichkar@gmail.com>
2019-11-13 10:52:28 +0200

    Initial commit

---
+++ InterviewExam/InterviewExam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InterviewExam.WebApiApplication" Language="C#" %>
---
+++ InterviewExam/IntreviewExam.ImportData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntreviewExam.ImportData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
